43503fde8d0f5b99bc77c7d1ee15b7181c830a17
Redd Caruana <redd@jongleurmt.com>
2021-12-05 15:05:58 +0100

    Player Mechanics
    + Movement
    + Jumping
    + Melee

---
+++ Assets/Scripts/Base/PlayerControllerBase.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Base class for character controls.
+/// </summary>
+public abstract class PlayerControllerBase : MonoBehaviour
+{
+    // The player input component.
+    private PlayerInput m_PlayerInput = null;
+
+    // The player index.
+    public int ID => m_PlayerInput == null ? -1 : m_PlayerInput.playerIndex;
+
+    // Input actions to bind.
+    private InputAction m_DirectionAction = null;
+    private InputAction m_InteractAction = null;
+    private InputAction m_JumpAction = null;
+    private InputAction m_MeleeAction = null;
+    private InputAction m_ThrowAction = null;
+
+    /// <summary>
+    /// Handles the input actions.
+    /// </summary>
+    /// <param name="context">The callback context given by each action.</param>
+    private void ActionTriggeredHandler(InputAction.CallbackContext context)
+    {
+        // The direction action.
+        if (context.action.Equals(m_DirectionAction) && context.performed)
+            Direction(context.ReadValue<Vector2>());
+        
+        // The interact action.
+        else if (context.action.Equals(m_InteractAction) && !context.performed)
+            Interact();
+
+        // The jump action.
+        else if (context.action.Equals(m_JumpAction) && !context.performed)
+            Jump();
+
+        // The melee action.
+        else if (context.action.Equals(m_MeleeAction) && !context.performed && !context.canceled)
+            Melee();
+            
+        // The throw action.
+        else if (context.action.Equals(m_ThrowAction) && !context.performed)
+            Throw();
+    }
+
+    /// <summary>
+    /// Binds the player input controls.
+    /// </summary>
+    /// <param name="playerInput">The player input controls.</param>
+    public void Bind(PlayerInput playerInput)
+    {
+        // Input setup.
+        m_PlayerInput = playerInput;
+        m_PlayerInput.notificationBehavior = PlayerNotifications.InvokeCSharpEvents;
+
+        // In case the current action map is null.
+        if (m_PlayerInput.currentActionMap == null)
+            m_PlayerInput.SwitchCurrentActionMap(m_PlayerInput.defaultActionMap);
+
+        // Action references.
+        m_DirectionAction = m_PlayerInput.currentActionMap.FindAction("Direction");
+        m_InteractAction = m_PlayerInput.currentActionMap.FindAction("Interact");
+        m_JumpAction = m_PlayerInput.currentActionMap.FindAction("Jump");
+        m_MeleeAction = m_PlayerInput.currentActionMap.FindAction("Melee");
+        m_ThrowAction = m_PlayerInput.currentActionMap.FindAction("Throw");
+
+        // Bind the actions.
+        m_PlayerInput.onActionTriggered += ActionTriggeredHandler;
+    }
+
+    /// <summary>
+    /// Removes all references to the player input.
+    /// </summary>
+    public void Unbind()
+    {
+        if (m_PlayerInput == null) return;
+        
+        // Clear the function.
+        m_PlayerInput.onActionTriggered -= ActionTriggeredHandler;
+
+        // Clean up the actions.
+        m_DirectionAction = null;
+        m_InteractAction = null;
+        m_JumpAction = null;
+        m_MeleeAction = null;
+        m_ThrowAction = null;
+
+        // Remove the input reference.
+        m_PlayerInput = null;
+    }
+
+    /// <summary>
+    /// The direction action.
+    /// </summary>
+    /// <param name="value">The vector value.</param>
+    protected virtual void Direction(Vector2 value)
+    { }
+
+    /// <summary>
+    /// The interact action.
+    /// </summary>
+    protected virtual void Interact()
+    { }
+
+    /// <summary>
+    /// The jump action.
+    /// </summary>
+    protected virtual void Jump()
+    { }
+
+    /// <summary>
+    /// The melee action.
+    /// </summary>
+    protected virtual void Melee()
+    { }
+
+    /// <summary>
+    /// The throw action.
+    /// </summary>
+    protected virtual void Throw()
+    { }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : PlayerControllerBase
+{
+    [Header("Testing")]
+    [SerializeField]
+    private PlayerInput m_BindInput = null;
+
+    [Header("Transform Manipulation")]
+    [SerializeField]
+    private float m_MotionSpeed = 10f;
+
+    [SerializeField]
+    private float m_RotationSpeed = 90f;
+
+    [SerializeField]
+    private float m_JumpForce = 800f;
+
+    [SerializeField]
+    private float m_DescentDrag = -1.5f;
+
+    [SerializeField]
+    private float m_KnockbackForce = 100f;
+
+    [Header("Links")]
+    [SerializeField]
+    private Animator m_MeleeAnimator = null;
+
+    // The mesh renderer components.
+    private MeshRenderer[] m_MeshRenderers = {};
+
+    // Whether or not the player is grounded.
+    private bool m_IsGrounded = true;
+
+    // The input direction vectors.
+    private Vector3 m_Direction = Vector3.zero;
+
+    // The target rotation.
+    private Vector3 m_TargetRotation = Vector3.zero;
+
+    // The rigidbody component.
+    private Rigidbody m_Rigidbody = null;
+
+    // The default drag.
+    private float m_DefaultDrag = 0f;
+
+    /// <summary>
+    /// Component caching.
+    /// </summary>
+    void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_MeshRenderers = GetComponentsInChildren<MeshRenderer>();
+
+        m_DefaultDrag = m_Rigidbody.drag;
+    }
+
+    /// <summary>
+    /// Property setup.
+    /// </summary>
+    void Start()
+    {
+        if (m_BindInput != null) Bind(m_BindInput);
+        m_TargetRotation = m_Rigidbody.rotation.eulerAngles;
+    }
+
+    // Physics handling.
+    void FixedUpdate()
+    {
+        // Character motion.
+        Vector3 velocity = m_Direction * m_MotionSpeed;
+        velocity.y = m_Rigidbody.velocity.y;
+        m_Rigidbody.velocity = velocity;
+
+        // Character rotation.
+        if (m_Direction != Vector3.zero)
+        {
+            Quaternion lookRot = Quaternion.LookRotation(m_Direction, Vector3.up);
+            m_Rigidbody.MoveRotation(Quaternion.Slerp(m_Rigidbody.rotation, lookRot, m_RotationSpeed * Time.fixedDeltaTime));
+        }
+
+        // Fall handler.
+        if (m_Rigidbody.velocity.y < 0 && !m_IsGrounded)
+            m_Rigidbody.drag = m_DescentDrag;
+        else
+            m_Rigidbody.drag = m_DefaultDrag;
+    }
+
+    // Collision Detection.
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            m_IsGrounded = true;
+        else if (collision.gameObject.CompareTag("MeleeArm"))
+        {
+            if (collision.contactCount == 0) return;
+
+            ContactPoint contact = collision.GetContact(0);
+            Vector3 direction = transform.position - contact.point;
+            direction.y = 1f;
+            direction.x *= 10f;
+
+            StartCoroutine(Invincibility(3, 0.15f));
+        }
+    }
+
+    // Collision detection.
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            m_IsGrounded = false;
+    }
+
+    // Direction handler.
+    protected override void Direction(Vector2 value)
+    {
+        m_Direction = new Vector3(value.x, 0, value.y);
+    }
+
+    // Jump handler.
+    protected override void Jump()
+    {
+        if (m_IsGrounded)
+            m_Rigidbody.AddForce(Vector3.up * m_JumpForce, ForceMode.Impulse);
+    }
+
+    // Melee handler.
+    protected override void Melee()
+    {
+        if (m_MeleeAnimator == null) return;
+        m_MeleeAnimator.SetBool("MeleeAlternate", !m_MeleeAnimator.GetBool("MeleeAlternate"));
+        m_MeleeAnimator.SetTrigger("Melee");
+    }
+
+    // Temporary invincibility.
+    protected IEnumerator Invincibility(int cycles, float transitionDuration)
+    {
+        gameObject.layer = LayerMask.NameToLayer("InvinciblePlayer");
+        for (int i = 0; i < cycles; i++)
+        {
+            foreach (MeshRenderer mr in m_MeshRenderers)
+                mr.enabled = false;
+
+            yield return new WaitForSeconds(transitionDuration);
+
+            foreach (MeshRenderer mr in m_MeshRenderers)
+                mr.enabled = true;
+            
+            yield return new WaitForSeconds(transitionDuration);
+        }
+        gameObject.layer = LayerMask.NameToLayer("Player");
+    }
+}
