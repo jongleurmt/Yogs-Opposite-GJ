cfcabe142c9419d64fa5cf180f01fb43e996ad69
ShaunVassallo <shaunvassallo@hotmail.com>
2021-12-06 19:04:21 +0100

    object pooling

--- Assets/Scripts/ObjectEffect.cs
+++ Assets/Scripts/ObjectEffect.cs
@@ -7,35 +7,77 @@ public class ObjectEffect : MonoBehaviour
 {
     public GameObject[] children;
 
-    public int damageValue;
-    public int heavyValue;
+    public enum DamageValue { Soft, Medium, Hard }
+    public DamageValue Damage;
+    public enum WeightValue { Light, Medium, Heavy }
+    public WeightValue Weight;
+    public int Force;
 
-    private bool held;
-    private void start()
+    public int health;
+
+    private Collider[] _coll;
+    private Rigidbody _rb;
+
+    [SerializeField]
+    private bool held = false;
+    private void Awake()
     {
-        held = false;
+        _coll = GetComponentsInChildren<Collider>();
+        _rb = GetComponent<Rigidbody>();
+
+        // add 1 to items amount in game manager
     }
 
-    private void Update()
+    public void PickUp(Transform hands)
     {
         //if it was picked up by player set held to true
+        foreach(var parts in _coll)
+        {
+            parts.enabled = false;
+        }
+        _rb.isKinematic = true;
+        
+        Debug.Log("picked up");
+        
+        transform.parent = hands;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+
+        held = true;
+    }
+
+    public void Throw()
+    {
+
+        foreach (var parts in _coll)
+        {
+            parts.enabled = true;
+        }
+        _rb.isKinematic = false;
+        transform.parent = null;
+
+        //add force
+        Vector3 v3Force = (Force * transform.forward) + (Force * transform.up);
+        _rb.velocity = v3Force;
     }
 
     private void OnCollisionEnter(Collision coll)
     {
+        //Debug.Log(coll.gameObject.name);
+
         if(held)
         {
             //put the code below here else object will be destroyed when spawning
-        }
-        foreach (var child in children)
-        {
-            child.AddComponent<Rigidbody>();
-            child.transform.parent = null;
+            foreach (var child in children)
+            {
+                child.AddComponent<Rigidbody>();
+                child.transform.parent = null;
 
-            Destroy(child, 2f);
-        }
+                Destroy(child, 2f);
+            }
 
-        Destroy(this.gameObject , 1f);
+            // subtrect 1 to items amount in game manager
+            Destroy(this.gameObject, 1f);
+        }        
     }
-
 }
--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class SpawnObjects : MonoBehaviour
 {
-    public GameObject[] items;
+    //public GameObject[] items;
     public Transform maxX, minX, maxZ, minZ, topY;
     public int minAmount, maxAmount;
     public int offset;
 
+
+    ObjectPooler objectPooler;
+
     private void Start()
     {
+        objectPooler = ObjectPooler.Instance;
+
         spawnItems();
     }
 
@@ -20,12 +25,16 @@ public class SpawnObjects : MonoBehaviour
 
         for (int i = 0; i < amount; i++)
         {
-            int itemRand = Random.Range(0, items.Length);
+            int itemRand = Random.Range(0, objectPooler.pools.Count);
 
             float randX = Random.Range(minX.position.x + offset, maxX.position.x - offset);
             float randZ = Random.Range(minZ.position.z + offset, maxZ.position.z - offset);
+            Vector3 newPosition = new Vector3(randX, topY.position.y, randZ);
 
-            Instantiate(items[itemRand], new Vector3(randX, topY.position.y, randZ), Quaternion.identity);
+            //randomli selects an object from the pool but it wants the tag name as in stings(objectPooler.pools[itemRand].tag)
+            objectPooler.SpawnFromPool(objectPooler.pools[itemRand].tag, newPosition, Quaternion.identity);
+
+            //Instantiate(items[itemRand], new Vector3(randX, topY.position.y, randZ), Quaternion.identity);
         }
     }
 }
